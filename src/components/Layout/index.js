37a3b4ccf8d3b6a2db89e4e6e5940898fd1ce3ff
Harry Chiu <harrychiu1106@gmail.com>
2020-03-31 12:09:49 +0800

    Change Grid's display in mobile

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {
+    Grid,
     makeStyles,
     CssBaseline,
+    Typography,
+    Divider,
 } from '@material-ui/core';
 
 import NavigationBar from './components/NavigationBar';
@@ -12,18 +15,38 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         background: '#f4f7fd',
     },
-    content: {
+    main: {
         width: '100%',
         marginLeft: 64,
-        padding: theme.spacing(2),
         [theme.breakpoints.down('sm')]: {
             marginLeft: 0,
+        },
+    },
+    content: {
+        padding: theme.spacing(2),
+        [theme.breakpoints.down('sm')]: {
             padding: theme.spacing(1),
         },
     },
     toolbar: {
         // 使 toolbar 和 NavigationBar 相同高度
         ...theme.mixins.toolbar,
+    },
+    footer: {
+        marginTop: theme.spacing(3),
+        padding: theme.spacing(1, 0),
+        background: '#ffffff',
+        borderTop: '1px solid rgba(0, 0, 0, 0.12)',
+    },
+    divider: {
+        margin: theme.spacing(0, 2),
+        [theme.breakpoints.down('xs')]: {
+            display: 'none',
+        },
+    },
+    link: {
+        textDecoration: 'none',
+        color: '#107ae4',
     },
 }));
 
@@ -50,9 +73,26 @@ const Layout = ({ children }) => {
                 open={open}
                 handleDrawerClose={handleDrawerClose}
             />
-            <main className={classes.content}>
+            <main className={classes.main}>
                 <div className={classes.toolbar} />
-                {children}
+                <div className={classes.content}>
+                    {children}
+                </div>
+                <footer className={classes.footer}>
+                    <Grid container justify="center" alignItems="center">
+                        <Grid item xs={12} sm="auto">
+                            <Typography variant="subtitle1" align="center">
+                                UI Designed by <a className={classes.link} href="https://dribbble.com/filipjustic">Filip Justić</a>
+                            </Typography>
+                        </Grid>
+                        <Divider orientation="vertical" flexItem className={classes.divider} />
+                        <Grid item xs={12} sm="auto">
+                            <Typography variant="subtitle1" align="center">
+                                Coded by <a className={classes.link} href="https://github.com/harry-chiu">Harry Chiu</a>
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </footer>
             </main>
         </div>
     );
